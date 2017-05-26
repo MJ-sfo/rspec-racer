be7a1af7054a9ea5477be2b1acd29ed79b5b8abb
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-25 22:25:54 -0700

    all three car rspec tests pass

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Car do
   before :each do
     @car = Car.new
   end  #  before :each
+
   describe '::new' do
     it 'sets the initial speed of a car to 0' do
       @car.speed.should eql 0
@@ -11,10 +12,16 @@ RSpec.describe Car do
   end
 
   describe '#speed' do
-    it 'has a speed getter method'
+    it 'has a speed getter method'  do
+      # @car.speed.should_not eql nil
+      expect(@car.speed).to be_truthy
+    end
   end
 
   describe '#accelerate' do
-    it 'increases the speed of car by a given amount'
+    it 'increases the speed of car by a given amount'  do
+      @car.accelerate(5).should eql 5
+    end
+
   end
 end   #  RSpec.describe
